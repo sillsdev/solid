d746813157cc6913a15474f67856b1821b880ede
Cambell Prince <cambell_prince@sil.org>
2007-11-07 09:15:39 +0000

    Fixed #103 Default button in marker settings dialog.

--- src/SolidGui/MarkerSettingsDialog.Designer.cs
+++ src/SolidGui/MarkerSettingsDialog.Designer.cs
@@ -57,8 +57,10 @@ namespace SolidGui
             // 
             // MarkerSettingsDialog
             // 
+            this.AcceptButton = this._closeButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this._closeButton;
             this.ClientSize = new System.Drawing.Size(622, 375);
             this.ControlBox = false;
             this.Controls.Add(this._closeButton);
