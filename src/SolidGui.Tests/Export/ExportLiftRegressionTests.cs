8e5c1c7d98c4b7fe403ab6d6969431dd7d9bfc1b
Cambell Prince <cambell_prince@sil.org>
2010-09-29 21:43:26 +0700

    ref #157 Lift export std hierarchy (ps/sn). Added test.

--- src/SolidGui.Tests/Export/ExportLiftRegressionTests.cs
+++ src/SolidGui.Tests/Export/ExportLiftRegressionTests.cs
@@ -64,6 +64,18 @@ namespace SolidGui.Tests.Export
                 setting.Mappings[1] = liftConcept;
             }
 
+            public void SetupMarker(string marker, string liftConcept, string writingSystem, string parent, bool infer)
+            {
+                var setting = SolidSettings.FindOrCreateMarkerSetting(marker);
+                setting.WritingSystemRfc4646 = writingSystem;
+                setting.Mappings[1] = liftConcept;
+                setting.StructureProperties.Add(new SolidStructureProperty(parent));
+                if (infer)
+                {
+                    setting.InferedParent = parent;
+                }
+            }
+
             public string LiftAsString()
             {
                 return File.ReadAllText(LiftPath);
@@ -101,6 +113,54 @@ namespace SolidGui.Tests.Export
             {
                 e.SetupMarker("lx", "lexicalUnit", "en");
                 e.SetupMarker("bw", "borrowedWord", "en");
+                var liftExporter = new LiftExporter();
+                liftExporter.Export(e.Dictionary.AllRecords, e.SolidSettings, e.LiftPath);
+                AssertThatXmlIn.String(e.LiftAsString()).HasAtLeastOneMatchForXpath("/lift/entry/trait[@name='etymology'][@value='BorrowedWord']");
+            }
+        }
+
+        [Test]
+        // http://projects.palaso.org/issues/show/157
+        public void Bug157_LiftExportPartOfSpeech_IsInLift()
+        {
+            string sfm = @"
+\lx d'ara
+\hm 1
+\ph dara
+\sd location, body
+\ps n
+\pn kb
+\sn 1
+\ge inside
+\gn dalam
+\va ra
+\sn 2
+\ge character; emotions; heart
+\de character, seat of emotions.
+\gn hati; pikiran
+\ps TAM
+\ge in process of; busy doing
+\gn dalam (keadaan)
+\ps Prep
+\pn kdep
+\ge in; inside
+\gn dalam; di dalam
+\dt 19/Jan/2008
+";
+            using (var e = new EnvironmentForTest(sfm))
+            {
+                e.SetupMarker("lx", "lexicalUnit", "vv");
+                e.SetupMarker("hm", "homonym", "en", "lx", false);
+                e.SetupMarker("ph", "pronunciation", "en-fonipa", "lx", false);
+                e.SetupMarker("sd", "semanticDomain", "en", "lx", false);
+                e.SetupMarker("ps", "grammi", "en", "lx", false);
+                e.SetupMarker("pn", "grammi", "nn", "ps", false);
+                e.SetupMarker("sn", "sense", "en", "ps", false);
+                e.SetupMarker("ge", "gloss", "en", "sn", true);
+                e.SetupMarker("de", "definition", "en", "sn", false);
+                e.SetupMarker("gn", "gloss", "nn", "sn", false);
+                e.SetupMarker("va", "variant", "vv", "lx", false);
+                e.SetupMarker("dt", "dateModified", "en", "lx", false);
                 var liftExporter = new LiftExporter();
                 liftExporter.Export(e.Dictionary.AllRecords, e.SolidSettings, e.LiftPath);
                 AssertThatXmlIn.String(e.LiftAsString()).HasAtLeastOneMatchForXpath("/lift/entry/trait[@name='etymology'][@value='BorrowedWord']");
