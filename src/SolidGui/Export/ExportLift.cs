9e57054f9b1d5636ab470b5a3adbacfc525b6156
John Hatton <john_hatton@sil.org>
2010-10-28 17:30:12 +1000

    Log export errors to a text file next to the output file.

--- src/SolidGui/Export/ExportLift.cs
+++ src/SolidGui/Export/ExportLift.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Text;
 using Palaso.DictionaryServices.Lift;
 using Palaso.DictionaryServices.Model;
@@ -17,87 +18,110 @@ namespace SolidGui.Export
         {
             var index = new Dictionary<string, SfmLiftLexEntryAdapter>();
             var liftLexEntries = new List<SfmLiftLexEntryAdapter>();
-
+            var logPath = outputFilePath+ ".exportErrors.txt";
+            if(File.Exists(logPath))
+            {
+                File.Delete(logPath);
+            }
             using (var dm = new LiftDataMapper(outputFilePath))
             {
                 // first pass
                 foreach (var sfmLexEntry in sfmLexEntries)
                 {
-                    var adaptedEntry = new SfmLiftLexEntryAdapter(dm, sfmLexEntry.LexEntry, solidSettings);
-                    liftLexEntries.Add(adaptedEntry);
-                    adaptedEntry.PopulateEntry();
-
-                    if (index.ContainsKey(adaptedEntry.SfmID)) // is a duplicate
+                    try
                     {
-                        // get the duplicated entry from index and change its name to SfmID_HomonymNumber
-                        var entry = index[adaptedEntry.SfmID];
-                        entry.SfmID = entry.SfmID + "_" + entry.HomonymNumber;
+                        var adaptedEntry = new SfmLiftLexEntryAdapter(dm, sfmLexEntry.LexEntry, solidSettings);
+                        liftLexEntries.Add(adaptedEntry);
+                        adaptedEntry.PopulateEntry();
 
-                        // add the new adapted entry to the index with name SfmID_HomonymNumber
-                        index.Add(adaptedEntry.SfmID + "_" + adaptedEntry.HomonymNumber, adaptedEntry);
+                        if (index.ContainsKey(adaptedEntry.SfmID)) // is a duplicate
+                        {
+                            // get the duplicated entry from index and change its name to SfmID_HomonymNumber
+                            var entry = index[adaptedEntry.SfmID];
+                            entry.SfmID = entry.SfmID + "_" + entry.HomonymNumber;
+
+                            // add the new adapted entry to the index with name SfmID_HomonymNumber
+                            index.Add(adaptedEntry.SfmID + "_" + adaptedEntry.HomonymNumber, adaptedEntry);
+                        }
+                        else
+                        {
+                            // add to dictionary
+                            index.Add(adaptedEntry.SfmID, adaptedEntry);
+                        }
                     }
-                    else
+                    catch (Exception error)
                     {
-                        // add to dictionary
-                        index.Add(adaptedEntry.SfmID, adaptedEntry);
+                        using (var log = System.IO.File.AppendText(logPath))
+                        {
+                            log.Write(sfmLexEntry.LexEntry.Name+": ");
+                            log.WriteLine(error);
+                        }
                     }
-
-
                 }
-
                 SfmLiftLexEntryAdapter targetLexEntry;
 
                 // second pass
                 foreach (var adaptedEntry in liftLexEntries)
                 {
-                    foreach (var relation in adaptedEntry.Relations)
+                    try
                     {
-                        // if the target is in the dictionary, add the relation, else post a note
-                        if (index.ContainsKey(relation.TargetID))
-                        {
-                            targetLexEntry = index[relation.TargetID];
-                            string guidOfTargetEntry = targetLexEntry.GUID;
-                            adaptedEntry.MakeRelation(guidOfTargetEntry, relation.Type);
-
-                        }
-                        else
-                        {
-                            // add an error note
-                            adaptedEntry.AddSolidNote(String.Format("Cannot find target: {0}", relation.TargetID));
-                            // add a note
-                            //adaptedEntry.LiftLexEntry.
-                        }
-                    }
-
-                    dm.SaveItem(adaptedEntry.LiftLexEntry);
-
-                    foreach (var subEntry in adaptedEntry.SubEntries)
-                    {
-                        foreach (var relation in subEntry.Relations)
+                        foreach (var relation in adaptedEntry.Relations)
                         {
                             // if the target is in the dictionary, add the relation, else post a note
                             if (index.ContainsKey(relation.TargetID))
                             {
                                 targetLexEntry = index[relation.TargetID];
                                 string guidOfTargetEntry = targetLexEntry.GUID;
-                                adaptedEntry.MakeRelation(guidOfTargetEntry, relation.Type); 
+                                adaptedEntry.MakeRelation(guidOfTargetEntry, relation.Type);
 
                             }
                             else
                             {
+                                // add an error note
+                                adaptedEntry.AddSolidNote(String.Format("Cannot find target: {0}", relation.TargetID));
                                 // add a note
                                 //adaptedEntry.LiftLexEntry.
                             }
                         }
 
-                        dm.SaveItem(subEntry.LiftLexEntry);
+                        dm.SaveItem(adaptedEntry.LiftLexEntry);
+
+                        foreach (var subEntry in adaptedEntry.SubEntries)
+                        {
+                            foreach (var relation in subEntry.Relations)
+                            {
+                                // if the target is in the dictionary, add the relation, else post a note
+                                if (index.ContainsKey(relation.TargetID))
+                                {
+                                    targetLexEntry = index[relation.TargetID];
+                                    string guidOfTargetEntry = targetLexEntry.GUID;
+                                    adaptedEntry.MakeRelation(guidOfTargetEntry, relation.Type);
+
+                                }
+                                else
+                                {
+                                    // add a note
+                                    //adaptedEntry.LiftLexEntry.
+                                }
+                            }
+
+                            dm.SaveItem(subEntry.LiftLexEntry);
+                        }
+
+                        // Got any potential relations?
+                        // if so
+                        // foreach one find your target in the index, get it's id, and make the relation
+
+
                     }
-
-                    // Got any potential relations?
-                    // if so
-                    // foreach one find your target in the index, get it's id, and make the relation
-
-                    
+                    catch (Exception error)
+                    {
+                        using (var log = System.IO.File.AppendText(logPath))
+                        {
+                            log.Write(adaptedEntry.SfmID + ": ");
+                            log.WriteLine(error);
+                        }
+                    }   
                 }
             }
 
