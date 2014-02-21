4cc1c01a74cba8ad7158057f4b69e6f7baefa089
Jon Coombs <jvcoombs@gmail.com>
2014-02-21 09:54:08 -0600

    replaced the license in the install wizard with MIT

--- build/TestInstallerBuild.bat
+++ build/TestInstallerBuild.bat
@@ -1,7 +1,12 @@
-call "c:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
+rem call "c:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
+call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 
-pushd c:\src\sil\solid\build
-MSbuild /target:installer /property:teamcity_build_checkoutDir=c:\src\sil\solid  /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="0.1.345.abcd" /property:Minor="1"
+rem pushd c:\src\sil\solid\build
+pushd C:\dev\solid\build
+
+rem MSbuild /target:installer /property:teamcity_build_checkoutDir=c:\src\sil\solid  /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="0.1.345.abcd" /property:Minor="1"
+MSbuild /target:installer /property:teamcity_build_checkoutDir=c:\dev\solid  /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="0.1.345.abcd" /property:Minor="1"
+
 popd
 PAUSE
 
