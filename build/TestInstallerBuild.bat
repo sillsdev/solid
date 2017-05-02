9342327ef9ee3c4e6eb1b73ff50867cf957a8e16
andrew-polk <andrew_polk@sil.org>
2017-05-02 16:34:53 -0400

    Change version to 0.20

--- build/TestInstallerBuild.bat
+++ build/TestInstallerBuild.bat
@@ -5,7 +5,7 @@ rem pushd c:\src\sil\solid\build
 pushd C:\dev\solid\build
 
 rem MSbuild /target:installer /property:teamcity_build_checkoutDir=c:\src\sil\solid  /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="0.1.345.abcd" /property:Minor="1"
-MSbuild /target:Upload /property:teamcity_build_checkoutDir=.. /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="0.1.345.abcd" /property:Minor="1"
+MSbuild /target:Upload /property:teamcity_build_checkoutDir=.. /property:teamcity_dotnet_nunitlauncher_msbuild_task="notthere" /property:BUILD_NUMBER="0.20.3456.abcd" /property:Minor="1"
 
 popd
 PAUSE
